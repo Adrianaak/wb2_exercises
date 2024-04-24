e46759dbaa13799ee1a9cb46b7c26546c09d17c1
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-04-24 14:32:15 -0500

    added new file and updates

--- math_scripts/area_of_circle.js
+++ math_scripts/area_of_circle.js
@@ -2,7 +2,7 @@
 let radius = 13
 
 //calculate the area
-let area = radius**2;
+let area = Math.PI * radius**2;
 
 //output the results to console
 console.log("The area of the circle is", area );
---
+++ math_scripts/rule_of_72.js
@@ -0,0 +1,8 @@
+//define the interest rate
+let interestRate = 7;
+
+//calculate the time it takes to double in value
+let yearsToDouble = 72  / interestRate;
+
+//output the results in the console
+console.log("It will take approximately", interestRate );
