552312a51d1e52eb59e0b2bfa0532125cd0688c7
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-04-24 11:53:15 -0500

    included updates

---
+++ math_scripts/net_worth.js
@@ -0,0 +1,10 @@
+//define variables for  totalAssets and totalDebts
+let totalAssets = 32600;
+let totalDebts = 3000;
+
+//calculate the netWorth
+let netWorth = totalAssets - totalDebts;
+
+
+//output the results to console
+console.log("The total due is" + netWorth);
