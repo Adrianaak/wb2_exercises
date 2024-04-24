b8385e13c4a00ad6b4159d299886a219e7c4db82
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-04-24 13:30:35 -0500

    added additional files

---
+++ math_scripts/tip_amount.js
@@ -0,0 +1,9 @@
+//variables created as inputs for calculation
+let billAmount = 75.25;
+let tipPercentage = 15;
+
+//do our calculations for the variables above
+let tipAmount = billAmount * (tipPercentage /100)
+
+//output the results to console
+console.log("The tip amount is" + tipAmount);
