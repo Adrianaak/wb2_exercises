1b5effd120b3baa74825e4d83adcf29fa2710cdb
Adriana Kendricks <kendricksadriana16@gmail.com>
2024-04-24 13:49:16 -0500

    added update to file

---
+++ math_scripts/area_of_circle.js
@@ -0,0 +1,8 @@
+//define the radius of the circle
+let radius = 13
+
+//calculate the area
+let area = radius**2;
+
+//output the results to console
+console.log("The area of the circle is", area );
